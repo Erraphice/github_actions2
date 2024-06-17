2bb49b03403122da1702042caf832a8ec361f6fb
Erraphice <lagierraphael75@gmail.com>
2024-06-17 09:10:54 +0000

    modif 3 test.py

--- file_test.py
+++ file_test.py
@@ -1,21 +1,21 @@
 import pytest
 
 def test_calc_addition():
-	# fpnction test pour le résultat de 2+4
-	output = 2+4
-	assert output ==6
+    # fpnction test pour le résultat de 2+4
+    output = 2+4
+    assert output == 6
 
 def test_calc_substraction():
-	# fonction de résultat 2-4
-	output= 2-4
-	assert output==-2
+    # fonction de résultat 2-4
+    output = 2-4
+    assert output == -2
 
 def test_calc_multiply():
-	#fonction test de rs 2*4
-	output= 2*4
-	assert output==8
+#fonction test de rs 2*4
+    output = 2*4
+    assert output == 8
 
 def test_coucou():
-	#fonction qui tes si le res renvoie 'Hello'
-	output ='Hello'
-	assert output == "Hello"
+#fonction qui tes si le res renvoie 'Hello'
+    output = 'Hello'
+    assert output == "Hello"
